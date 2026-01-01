b78883cc12d2e0391a778f6a40b9d9bdd00a5a11
Kemal Mešić <153745485+mesicc@users.noreply.github.com>
2026-01-01 17:47:01 +0100

    Updaate section

--- js/script.js
+++ js/script.js
@@ -1,19 +1,10 @@
-window.addEventListener('scroll', function() {
-    const navbar = document.getElementById('navbar');
-    
-    // Ako se skrola više od 50px od vrha
-    if (window.scrollY > 50) {
-        navbar.classList.add('scrolled');
-    } else {
-        navbar.classList.remove('scrolled');
-    }
-});
-
-// Opcionalno: Hamburger menu funkcionalnost
 const menuToggle = document.getElementById('mobile-menu');
 const navLinks = document.querySelector('.nav-links');
 
 menuToggle.addEventListener('click', () => {
-    // Ovdje možeš dodati logiku za otvaranje mobilnog menija
-    alert('Meni otvoren!'); 
+    navLinks.classList.toggle('active');
+    // Menjanje ikonice bars u X
+    const icon = menuToggle.querySelector('i');
+    icon.classList.toggle('fa-bars');
+    icon.classList.toggle('fa-times');
 });
